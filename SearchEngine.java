642f88cd697eeea72a1a7f06e13f28948cccc397
dairelia <114545141+dairelia@users.noreply.github.com>
2022-10-07 00:51:01 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,26 +1,59 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+    ArrayList<String> listOfString = new ArrayList<String> ();
 
+    public String getList(ArrayList<String> someList){
+        
+        if(someList.size() == 0){
+            return "empty list";
+        }
+        String temp = someList.get(0);
+        for(int i = 0; i < someList.size(); i++){
+            temp = temp + ", " + someList.get(i);
+        }
+        return temp; 
+    }
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
+            if(listOfString.size()!= 0){
+                return new Handler().getList(listOfString);
             }
+            return "empty list";
+        } 
+
+        else if (url.getPath().equals("/add")) {
+            String[] s = url.getQuery().split("=");
+            if(s[0].equals("s")){
+                listOfString.add(s[1]);
+                return s[1] + " added, now the list is " + new Handler().getList(listOfString);
+            }
+            return new Handler().getList(listOfString);
+        } 
+
+        else if (url.getPath().equals("/search")) {
+            String[] s = url.getQuery().split("=");
+            ArrayList<String> searchList = new ArrayList<String>();
+            if(s[0].equals("s")){
+                for(int i = 0; i < listOfString.size(); i++){
+                    if(listOfString.get(i).contains(s[1])){
+                        searchList.add(listOfString.get(i));
+                    }
+                }
+                return new Handler().getList(searchList);
+            }
+        else{
+            return "Query not found";
+        }
+
+        } 
+
+        else {
             return "404 Not Found!";
         }
     }
